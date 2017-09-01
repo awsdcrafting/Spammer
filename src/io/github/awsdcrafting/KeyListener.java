5a8f372694d7bb8113811af864ef4deba3ec5db9
awsdcrafting <scisneromam@t-online.de>
2017-09-01 22:25:33 +0200

    fixed dependencies

---
+++ src/io/github/awsdcrafting/KeyListener.java
@@ -0,0 +1,25 @@
+package io.github.awsdcrafting;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+/**
+ * Created by scisneromam on 10.08.2017.
+ */
+public class KeyListener implements NativeKeyListener
+{
+
+	@Override
+	public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent)
+	{
+
+	}
+	@Override
+	public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent)
+	{
+
+	}
+	@Override
+	public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent)
+	{
+
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,26 @@
 package main;
+import io.github.awsdcrafting.KeyListener;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
 /**
  * Created by scisneromam on 10.08.2017.
  */
 public class Main
 {
+	public static void main(String[] args)
+	{
+		try
+		{
+			GlobalScreen.registerNativeHook();
+		} catch (NativeHookException ex)
+		{
+			System.err.println("There was a problem registering the native hook.");
+			System.err.println(ex.getMessage());
 
+			System.exit(1);
+		}
+
+		GlobalScreen.addNativeKeyListener(new KeyListener());
+	}
 }
+
